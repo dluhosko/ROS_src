14dac2bba7d432d047ea2668f405311de1e2c78e
senso-viktor <viktordluhos@gmail.com>
2018-04-24 09:51:50 +0200

    SENSODRIVE - node for publishing cylinders is publishing cylinders to moveit  ->TODO : add functions to hide and display cylinders
    Signed-off-by: senso-viktor <viktordluhos@gmail.com>

--- scara_v2_moveit_api/src/multiple_cylinder_publisher_scara_v3.cpp
+++ scara_v2_moveit_api/src/multiple_cylinder_publisher_scara_v3.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "cube_node");
     ros::NodeHandle n, node_gripper;
     //ros::Subscriber sub = n.subscribe("gripper_state_topic", 1000, chatterCallback);
-    ros::Rate loop_rate(5);
+    ros::Rate loop_rate(30);
 
     ros::NodeHandle nn, node_handle;
     ros::Publisher vis_pub = node_handle.advertise<visualization_msgs::Marker>( "visualization_marker", 1 );
@@ -17,10 +17,13 @@ int main(int argc, char **argv){
 
 
     while(ros::ok()) {
+        ROS_INFO_ONCE("[CYLINDER PUBLISHER NODE]: Started publishing cylinders");
 
-
-
-
+        for (int i=1;i<=8;i++){
+            generate_pick_cylinders(&vis_pub,i,0);
+            generate_place_cylinders(&vis_pub,i,0);
+            generate_attached_cylinder(&vis_pub,true);
+        }
         ros::spinOnce();
         loop_rate.sleep();
     }
